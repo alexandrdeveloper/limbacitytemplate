569ef7baa40e20562f40b4b3c9d87fea8243ce50
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-08-24 20:34:51 +0300

    animations

--- js/common.js
+++ js/common.js
@@ -1,4 +1,5 @@
 $(function() {
+	$('.authorise-form').hide();
 
 	$('.catalog-list').on('click', 'a:not(.catalog__link_active)', function(e) {
 		e.preventDefault();
@@ -12,8 +13,8 @@ $(function() {
 	$('.navbar-catalog__init').on('click', function(e) {
 		e.preventDefault();		
 		$('.menu-toggle').removeClass('menu-toggle_active').addClass('menu-toggle_return');
-		$('.catalog').toggleClass('catalog_visible');
-					
+		$('.catalog').slideToggle();	
+		$('.navbar-catalog__init-burger').toggleClass('navbar-catalog__init-burger_active');				
 	});
 
 
@@ -39,25 +40,26 @@ $(function() {
 		var div = $(".catalog__container, .navbar-catalog__init"); 
 		if (!div.is(e.target)
 		    && div.has(e.target).length === 0) {
-			$('.catalog').removeClass('catalog_visible');
+			$('.catalog').slideUp();
+			$('.navbar-catalog__init-burger').removeClass('navbar-catalog__init-burger_active');
 		}
-	});
-
-
+	})
+	
 
 	$('.header-top__authorise-enter').on('click', function(e) {
 		e.preventDefault();
-		$('.authorise-form').toggleClass('authorise-form_visible');			
+		$('.authorise-form').slideToggle(300).css({'display': 'flex'});			
 	});
+
 
 	$(document).mouseup(function (e){ 
-		var div = $(".authorise-form"); 
+		var div = $(".authorise-form, .header-top__authorise-enter"); 
 		if (!div.is(e.target)
 		    && div.has(e.target).length === 0) {
-			$('.authorise-form').removeClass('authorise-form_visible');
+			$('.authorise-form').slideUp(300);
+
 		}
 	});
-
 
 
 	$(document).mouseup(function (e){ 
@@ -95,6 +97,19 @@ $(function() {
 		asNavFor: $('.image-slider'),
 
 	});
+
+	let headerNavbar = $('.header-navbar');
+	let body = $('body');
+
+	 $(document).on('scroll', function () {
+        var position = body.scrollTop(),
+        navbar_position = headerNavbar.offset().top;
+        if (position - navbar_position < 0) { 
+            headerNavbar.addClass('header-navbar_fixed');
+        } else {
+            headerNavbar.removeClass('header-navbar_fixed');
+        }
+    });
 
 });
 	
